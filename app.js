f677f4ddafcf801a4d4870a2c4f9a01bf16c9d7b
aurel deac <aureludeac@yahoo.com>
2014-10-18 18:01:01 +0300

    mongodb + login added

--- app.js
+++ app.js
@@ -4,11 +4,30 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/users');
+
 var app = express(); 
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+var Schema = mongoose.Schema;
+var UserDetail = new Schema({
+      username: String,
+      password: String
+    }, {
+      collection: 'users'
+    });
+var UserDetails = mongoose.model('userInfo', UserDetail);
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +43,63 @@ app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+// app.use('/users', users);
+
+
+// passport login
+
+
+app.get('/login', function(req, res) {
+  res.render('login'); 
+});
+
+app.post('/login',
+  passport.authenticate('local', {
+    successRedirect: '/loginSuccess',
+    failureRedirect: '/loginFailure'
+  })
+);
+ 
+app.get('/loginFailure', function(req, res, next) {
+  res.send('Failed to authenticate');
+});
+ 
+app.get('/loginSuccess', function(req, res, next) {
+   res.render('index', { username: global.username, title: 'binelucrat' });
+});
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+ 
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.use(new LocalStrategy(function(username, password, done) {
+  process.nextTick(function() {
+    
+    global.username = username;
+
+    UserDetails.findOne({
+      'username': username, 
+    }, function(err, user) {
+      if (err) {
+        return done(err);
+      }
+ 
+      if (!user) {
+        return done(null, false);
+      }
+ 
+      if (user.password != password) {
+        return done(null, false);
+      }
+ 
+      return done(null, user);
+    });
+  });
+}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -45,6 +120,8 @@ if (app.get('env') === 'development') {
             error: err
         });
     });
+
+    // mongoose.connect();
 }
 
 // production error handler
