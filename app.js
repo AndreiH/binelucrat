185adcab283200f82915cb11c25b47655f8354c0
aurel deac <aureludeac@yahoo.com>
2014-11-02 22:09:09 +0200

    refactoring

--- app.js
+++ app.js
@@ -1,142 +1,41 @@
-var express = require('express');
+var Express = require('express');
+var Mongoose = require('mongoose');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+var bodyParser = require("body-parser");
+var port = 3000;
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+Mongoose.connect('mongodb://localhost/users');
 
-var mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/users');
-mongoose.connect('mongodb://aurel:logica00@linus.mongohq.com:10079/app30642759/users');
+var app = Express();
 
-var app = express(); 
+app.use(Express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use(passport.initialize());
-app.use(passport.session());
+var models = ["Email", "User"];
 
-// var config = require('./config')();
-// http.createServer(app).listen(config.port, function(){
-//     console.log('Express server listening on port ' + config.port);
-// });
-
-// database connection for login
-var Schema = mongoose.Schema;
-var User = new Schema({
-      username: String,
-      password: String
-    }, {
-      collection: 'users'
-    });
-var users = mongoose.model('userInfo', User);
-
+// initialize models
+models.forEach(function(model) {
+  require("./models/" + model + "Model")(Mongoose);
+  require("./routes/" + model + "Router")(app, Mongoose);
+});
+require("./routes/LoginRouter")(app, Mongoose);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hjs'); 
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(require('less-middleware')(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'public')));
+// var User = Mongoose.models.User;
+// var UserManager = require("./managers/UserManager")(Mongoose);
 
-app.use('/', routes);
-// app.use('/users', users);
+// app.get('/editedUsers', function(req, res) {
+//  User.find(function(error, result) {
+//     for (var i = 0; i < result.length; i++) {
+//       result[i].username = UserManager.myFancyFunction(result[i].username);
+//     }
+//     res.status(200).json(result);
+//   });
+// });
 
-
-// passport login
-app.get('/login', function(req, res) {
-  res.render('login'); 
-});
-
-app.post('/login',
-  passport.authenticate('local', {
-    successRedirect: '/loginSuccess',
-    failureRedirect: '/loginFailure'
-  })
-);
- 
-app.get('/loginFailure', function(req, res, next) {
-  res.send('Failed to authenticate');
-});
- 
-app.get('/loginSuccess', function(req, res, next) {
-   res.render('index', { username: global.username, title: 'binelucrat' });
-});
-
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
- 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
-
-passport.use(new LocalStrategy(function(username, password, done) {
-  process.nextTick(function() {
-    
-    global.username = username;
-
-    users.findOne({
-      'username': username, 
-    }, function(err, user) {
-      if (err) {
-        return done(err);
-      }
- 
-      if (!user) {
-        return done(null, false);
-      }
- 
-      if (user.password != password) {
-        return done(null, false);
-      }
- 
-      return done(null, user);
-    });
-  });
-}));
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-
-    // mongoose.connect();
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-
-module.exports = app;
+app.listen(port);
+console.log('Listening on port', port);
